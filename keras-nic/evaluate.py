37f3b0f05c928da55614bcff3cf7908718c267e0
Carlos Antonetti <carlosantonetti24@gmail.com>
2018-12-09 19:58:14 -0500

    keras nic documentation

--- keras-nic/evaluate.py
+++ keras-nic/evaluate.py
@@ -58,90 +58,82 @@ def generate_desc(model, tokenizer, photo, max_length):
 # evaluate the skill of the model
 def evaluate_model(model, descriptions_list, photos, tokenizer, max_length):
 	actual, predicted = list(), list()
-	# step over the whole set
+	
+	# variables to calculate the avg bleu
 	total_1_blue_score = 0.00
 	total_4_blue_score = 0.00
 	img_count = 0
 
-	for key, descriptions in descriptions_list.items():
-		print key
-		label_desc = list()
-		blue_score = 0
-		predict = generate_desc(model, tokenizer, photos[key], max_length).replace('startseq','').replace('endseq','')
+	# lood through each image
+	for image_id, descriptions in descriptions_list.items():
+		print image_id
+		label_desc = list() # reset reference list for bleu score
+		
+		# predict a caption
+		predicted_caption = generate_desc(model, tokenizer, photos[image_id], max_length).replace('startseq','').replace('endseq','')
+		print 'Predicted: ' + predicted_caption
+		
+		# print all actual images and store them to calculate bleu
 		for desc in descriptions:
 			print 'Actual: ' + desc
 			label_desc.append(desc.replace('startseq','').replace('endseq','').split())
 			# max 5 desc per image
 			if len(actual) >= 5:
 				break
+
 		# calculate image BLEU score
-		print 'Predicted: ' + predict
-		blue_1_score = sentence_bleu(label_desc, predict.split(), smoothing_function=SmoothingFunction().method1)
-		blue_4_score = sentence_bleu(label_desc, predict.split(), smoothing_function=SmoothingFunction().method4)
+		blue_1_score = sentence_bleu(label_desc, predicted_caption.split(), smoothing_function=SmoothingFunction().method1)
+		blue_4_score = sentence_bleu(label_desc, predicted_caption.split(), smoothing_function=SmoothingFunction().method4)
 		print 'Blue 1 Score: %f' % blue_1_score
 		print 'Blue 4 Score: %f' % blue_4_score
+		
+		# to calculate average bleu
 		total_1_blue_score = total_1_blue_score + blue_1_score
 		total_4_blue_score = total_4_blue_score + blue_4_score
 		img_count = img_count + 1
-	# calculate total BLEU score
+	
+	# calculate avg BLEU score
 	bleu_1 = (total_1_blue_score / img_count) * 100.00
 	bleu_4 = (total_4_blue_score / img_count) * 100.00
 	print 'Avg BLUE-1 Score: %f' % bleu_1
 	print 'Avg BLUE-4 Score: %f' % bleu_4
 	return (bleu_1, bleu_4)
 
-# # load dev set
-# filename = 'datasets/Flickr8k_text/Flickr_8k.devImages.txt'
-# dataset = load_set(filename)
-# print('Dataset: %d' % len(dataset))
-
-# # train-test split
-# train, test = train_test_split(dataset)
-
-# my change
+# load descriptions
 train = load_set('datasets/Flickr8k_text/Flickr_8k.trainImages.txt')
-# my change
 test = load_set('datasets/Flickr8k_text/Flickr_8k.testImages.txt')
-
-# descriptions
 train_descriptions = load_clean_descriptions('descriptions.txt', train)
 test_descriptions = load_clean_descriptions('descriptions.txt', test)
 flat_list_train_descriptions = [desc for sublist in train_descriptions.values() for desc in sublist]
 flat_list_test_descriptions = [desc for sublist in test_descriptions.values() for desc in sublist]
 print('Descriptions: train=%d, test=%d' % (len(flat_list_train_descriptions), len(flat_list_test_descriptions)))
-# photo features
+
+
+# load photo features
 features_file = 'processed-data/features.pkl'
-train_features = load_photo_features(features_file, train)
 test_features = load_photo_features(features_file, test)
-print('Photos: train=%d, test=%d' % (len(train_features), len(test_features)))
+print('Photos: test=%d' % len(test_features))
+
 # prepare tokenizer
 tokenizer = create_tokenizer(flat_list_train_descriptions)
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
+
 # determine the maximum sequence length
 max_length = max(len(s.split()) for s in list(flat_list_train_descriptions))
 print('Description Length: %d' % max_length)
  
-
+# load YAML and create model
 model_name = 'baseline_model'
 model_yalm_file = 'processed-data/'+model_name+'.yalm'
-
-# load YAML and create model
 yaml_file = open(model_yalm_file, 'r')
 loaded_model_yaml = yaml_file.read()
 yaml_file.close()
 loaded_model = model_from_yaml(loaded_model_yaml)
+
 # load weights into new model
 loaded_model.load_weights("processed-data/model.h5")
 print("Loaded model from disk")
 
 # evaluate loaded model on test data
-# loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 score = evaluate_model(loaded_model, test_descriptions, test_features, tokenizer, max_length)
-
-# # generate description
-# key = "3482974845_db4f16befa"
-# predicted = generate_desc(loaded_model, tokenizer, test_features[key], max_length)
-# # store actual and predicted
-# # print('Actual:    %s' % desc)
-# print('Predicted: %s' % predicted)
--- keras-nic/train.py
+++ keras-nic/train.py
@@ -4,39 +4,31 @@ from datetime import datetime
 import keras
 import os
 
-# # load dev set
-# filename = 'datasets/Flickr8k_text/Flickr_8k.devImages.txt'
-# dataset = load_set(filename)
-# print('Dataset: %d' % len(dataset))
-
-# # train-test split
-# train, test = train_test_split(dataset)
-
-# my change
+# load (image, description) files
 train = load_set('datasets/Flickr8k_text/Flickr_8k.trainImages.txt')
-# my change
 test = load_set('datasets/Flickr8k_text/Flickr_8k.testImages.txt')
 
-# descriptions
+# load descriptions
 train_descriptions = load_clean_descriptions('descriptions.txt', train)
 test_descriptions = load_clean_descriptions('descriptions.txt', test)
+
+# flatten the description to a single list of descriptions
 flat_list_train_descriptions = [desc for sublist in train_descriptions.values() for desc in sublist]
 flat_list_test_descriptions = [desc for sublist in test_descriptions.values() for desc in sublist]
-'''
-for sublist in l:
-    for item in sublist:
-        flat_list.append(item)
-'''
+
 print('Descriptions: train=%d, test=%d' % (len(flat_list_train_descriptions), len(flat_list_test_descriptions)))
-# photo features
+
+# load photo features
 features_file = 'processed-data/features.pkl'
 train_features = load_photo_features(features_file, train)
 test_features = load_photo_features(features_file, test)
 print('Photos: train=%d, test=%d' % (len(train_features), len(test_features)))
+
 # prepare tokenizer
 tokenizer = create_tokenizer(flat_list_train_descriptions)
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
+
 # determine the maximum sequence length
 max_length = max(len(s.split()) for s in list(flat_list_train_descriptions))
 print('Description Length: %d' % max_length)
@@ -51,8 +43,8 @@ n_epochs_joint = 30
 n_photos_per_update = 5
 '''
 
-n_epochs_lstm = 70
-n_epochs_joint = 30
+n_epochs_lstm = 1
+n_epochs_joint = 1
 n_photos_per_update = 20
 n_batches_per_epoch = int(len(train) / n_photos_per_update)
 n_repeats = 1 #3
@@ -65,6 +57,7 @@ tbCallBack = keras.callbacks.TensorBoard(log_dir=tbLogDir, histogram_freq=0, wri
 # run experiment
 train_results, test_results = list(), list()
 for i in range(n_repeats):
+	
 	# define the model
 	model = define_model(vocab_size, max_length)
 
@@ -78,35 +71,24 @@ for i in range(n_repeats):
 	# fit model
 	model.fit_generator(data_generator(train_descriptions, train_features, tokenizer, max_length, n_photos_per_update), steps_per_epoch=n_batches_per_epoch, epochs=n_epochs_lstm, verbose=verbose, callbacks=[tbCallBack])
 
-	# train lstm and image layers besides the sentence embedding
+	# train all layers
 	for layer in model.layers:
-		# if layer.name not in ["embedding_1", "lstm_1", "time_distributed_1"]
 		layer.trainable = True
 
 	print(model.summary())
 
 	# fit model
 	model.fit_generator(data_generator(train_descriptions, train_features, tokenizer, max_length, n_photos_per_update), steps_per_epoch=n_batches_per_epoch, epochs=n_epochs_joint, verbose=verbose, callbacks=[tbCallBack])
-	# evaluate model on training data
-	train_score = evaluate_model(model, train_descriptions, train_features, tokenizer, max_length)
-	test_score = evaluate_model(model, test_descriptions, test_features, tokenizer, max_length)
-	# store
-	train_results.append(train_score)
-	test_results.append(test_score)
-	print('>%d: train=%f test=%f' % ((i+1), train_score, test_score))
-	
-    # serialize model to YAML
 
+    # save model to YAML
 	model_yalm_file = 'processed-data/'+model_name+'.yalm'
 	if os.path.exists(model_yalm_file):
 		os.remove(model_yalm_file)
-
 	model_yaml = model.to_yaml()
 	with open(model_yalm_file, "w") as yaml_file:
 	    yaml_file.write(model_yaml)
 
 	# serialize weights to HDF5
-
 	model_weight_file = "processed-data/model.h5"
 	if os.path.exists(model_weight_file):
 		os.remove(model_weight_file)
@@ -114,9 +96,3 @@ for i in range(n_repeats):
 	model.save_weights(model_weight_file)
 	print("Saved model to disk")
     
-# save results to file
-df = DataFrame()
-df['train'] = train_results
-df['test'] = test_results
-print(df.describe())
-df.to_csv('processed-data/'+model_name+'.csv', index=False)
